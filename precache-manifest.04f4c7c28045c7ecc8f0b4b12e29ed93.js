9590eb080f8382895dee93b79dc77ac830015e72
doyen <jardinerodney@gmail.com>
2019-04-09 20:16:31 +0100

    Updates

---
+++ precache-manifest.04f4c7c28045c7ecc8f0b4b12e29ed93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7a01ecba0d66bd4c03b",
+    "url": "/knockknockadminreactfb/static/js/runtime~main.e3e75c33.js"
+  },
+  {
+    "revision": "48244f862ca9741c04c1",
+    "url": "/knockknockadminreactfb/static/js/main.99133fb6.chunk.js"
+  },
+  {
+    "revision": "8667b85d54a39cd6a98a",
+    "url": "/knockknockadminreactfb/static/js/2.f39386bf.chunk.js"
+  },
+  {
+    "revision": "48244f862ca9741c04c1",
+    "url": "/knockknockadminreactfb/static/css/main.1bccc897.chunk.css"
+  },
+  {
+    "revision": "c683a9796dbeca5467ea905dd7a3183c",
+    "url": "/knockknockadminreactfb/index.html"
+  }
+];
