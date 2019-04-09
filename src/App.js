32bff5cf46644500070eb5fa93856eacaf91e6e3
doyen <jardinerodney@gmail.com>
2019-04-09 13:17:08 +0100

    font style added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
 // links
 import Navbar from "./components/layout/Navbar";
 import Dashboard from "./components/layout/Dashboard";
